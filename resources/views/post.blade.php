fceb174059e2c15f90f6b3d40cd641f569cf9f23
gumbalowsky <weremij2@o2.pl>
2019-12-08 23:11:37 +0100

    iphone ad

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,8 +3,10 @@
 <link rel="stylesheet" href="{{asset('assets/fonts/font-awesome.min.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/Team-Boxed.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/Highlight-Phone.css')}}">
 @endsection
 
 @section('content')
     @extends('layouts.list')
+    @extends('ads.iphone')
 @endsection

9d8f19f3d43f3e7f98fc1d10c0d4c13419cc6d16
gumbalowsky <weremij2@o2.pl>
2019-12-08 23:20:57 +0100

    fix

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -30,8 +30,8 @@
                   @yield('content')
             </div>
         </section>
+        @extends('layouts.footer')
     </main>
-    @extends('layouts.footer')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/pikaday.min.js"></script>
